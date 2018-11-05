d3206080589d134b9481d6ded48e29b7052bb141
lichun <chunli@sf-express.com>
2018-11-05 17:05:59 +0800

    del

--- index.js
+++ index.js
@@ -1,75 +1,49 @@
 #!/usr/bin/env node
 'use strict'
 
-const path = require('path')
-const fs = require('fs')
 const commander = require('commander')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 
-const config = require('./util/config').getConfig()
-const moduleModeConfig = config.getBasicConfig('module')
-
-const runGenerateModule = require('./lib/module')
 const runGeneratePage = require('./lib/page')
 
 commander
   .version('0.1.0')
   .option('-h, --help', 'show help')
-  .option('-m, --module [value]', 'generate a new module')
-  .option('-p, --page [value]', 'generate a new page')
+  .option('-c, --create [value]', 'create a new page')
   .parse(process.argv);
 
 commander.on('--help', () => {
   console.log('  Examples:')
   console.log()
   console.log(chalk.gray('    # create a new module'))
-  console.log('    $ npm run init -m moduleName')
-  console.log()
-  console.log(chalk.gray('    # create a new page'))
-  console.log('    $ npm run init -p moduleName')
+  console.log('    $ npm run init -c moduleName')
   console.log()
 })
 
-if (commander.module && !commander.page) {
-  generateModule(commander.module)
-} else if (commander.page) {
-  if (commander.module) {
-    generatePage(commander.module, commander.page)
-  } else {
-    inquirer.prompt([{
-      type: 'input',
-      message: 'Please enter the parent module name: ',
-      name: 'ok'
-    }]).then(answers => {
-      if (answers.ok) {
-        generatePage(answers.ok, commander.page)
-      }
-    })
-  }
-}
-
-function generateModule(moduleName) {
-  const to = path.join(moduleModeConfig.destBasePath, moduleName.toLowerCase());
-
-  if (fs.existsSync(to)) {
-    inquirer.prompt([{
-      type: 'confirm',
-      message: 'Target directory exists. Continue?',
-      name: 'ok'
-    }]).then(answers => {
-      if (answers.ok) {
-        runGenerateModule(moduleName)
-      }
-    })
-  } else {
-    runGenerateModule(moduleName)
-  }
+if (commander.create) {
+  inquirer.prompt([{
+    type: 'confirm',
+    message: 'Does it has a parent module?',
+    name: 'ok'
+  }]).then(answers => {
+    if (answers.ok) {
+      inquirer.prompt([{
+        type: 'input',
+        message: 'Please enter the parent module key: ',
+        name: 'key'
+      }]).then(module => {
+        if (module.key) {
+          console.log(module.key, commander.create)
+          generatePage(module.key, commander.create)
+        }
+      })
+    } else {
+      generatePage(null, commander.create)
+    }
+  })
 }
 
 function generatePage(moduleName, pageName) {
-  // todo moduleName大小写待完善
-  const to = path.join(moduleModeConfig.destBasePath, moduleName);
-
   runGeneratePage(moduleName, pageName)
 }
