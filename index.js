964990f0897e0207b1d36a675b916cfc4b998ee8
lichun <chunli@sf-express.com>
2018-11-07 17:02:28 +0800

    publish 0.1.4

--- index.js
+++ index.js
@@ -12,43 +12,43 @@ const runGeneratePage = require('./lib/page');
 const Templates = require('./lib/templates');
 
 commander
-  .version('0.1.1')
+  .version('0.1.4')
   .option('-c, --create [value]', 'create a new page')
   .parse(process.argv);
 
-// 获取可选模板类型；
-const templates = Templates.getTemplates();
+Templates.checkTemplateVersion()
+  .then(Templates.getTemplatesInfo)
+  .then(({ templates, packagePath }) => {
+    inquirer.prompt([{
+      type: 'list',
+      message: 'Please choose the template',
+      default: config.defaultTemplate,
+      choices: templates,
+      name: 'template',
+    }]).then(({ template }) => {
+      const templateInfo = {
+        template: template,
+        packagePath: packagePath,
+      };
 
-console.log(chalk.yellow('The template version is ' + Templates.checkTemplateVersion()) );
-console.log();
-
-// 用户选择模板
-inquirer.prompt([{
-  type: 'list',
-  message: 'Please choose the template',
-  default: config.defaultTemplate,
-  choices: templates,
-  name: 'template',
-}]).then(({ template }) => {
-  inquirer.prompt([{
-    type: 'confirm',
-    message: 'Does it has a parent module?',
-    name: 'type'
-  }]).then(({ type }) => {
-    if (type) {
       inquirer.prompt([{
-        type: 'input',
-        message: 'Please enter the parent module key: ',
-        name: 'parent'
-      }]).then(({ parent }) => {
-        if (parent) {
-          console.log(template, parent, commander.create)
-          runGeneratePage(template, parent, commander.create)
+        type: 'confirm',
+        message: 'Does it has a parent module?',
+        name: 'type'
+      }]).then(({ type }) => {
+        if (type) {
+          inquirer.prompt([{
+            type: 'input',
+            message: 'Please enter the parent module key: ',
+            name: 'parent'
+          }]).then(({ parent }) => {
+            if (parent) {
+              runGeneratePage(templateInfo, parent, commander.create)
+            }
+          })
+        } else {
+          runGeneratePage(templateInfo, null, commander.create)
         }
       })
-    } else {
-      console.log(template, null, commander.create)
-      runGeneratePage(template, null, commander.create)
-    }
+    })
   })
-})
